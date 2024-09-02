5772475d47bedf69e4ed5b45a37e981be66e5dd4
“Yash <syash5551@gmail.com>
2024-09-02 15:36:05 +0530

    update 1

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+```bash
+npm run dev
+# or
+yarn dev
+```
+
---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="footer">
+        Built by: &nbsp;
+        <a
+          href=""
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Yash Singh
+        </a>
+      </footer>
+    </>
+  )
+}
